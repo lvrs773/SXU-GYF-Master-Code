c237bfc4d5ec79b4ad356db0e3456adb11d9c49a
lvrs773 <2583604926@qq.com>
2025-11-29 22:06:27 +0800

    Delete Chapter 4 directory

--- Chapter 4/example.py
+++
@@ -1,7 +0,0 @@
-from llama_index.llms import Ollama
-
-llm = Ollama(model="llama3:8b", temperature=0, request_timeout=120.0) # llama3:8b, qwen2:7b, phi3:8b, gemma2:9b
-reasoner = MultiplexToTReasoner()
-question = "Why is the sky blue?"
-result = await reasoner.process_question(question, llm)
-print(reasoner.format_response(result))
